fd5981d4552cf212d8a232471092cfb0d1b167e9
Enwteyn93 <enwteyn93@gmail.com>
2021-03-31 17:35:45 +0500

    first commit

---
+++ Core/Inc/arp.h
@@ -0,0 +1,11 @@
+#ifndef ARP_H_
+#define ARP_H_
+//--------------------------------------------------
+#include "stm32f1xx_hal.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "enc28j60.h"
+#include "net.h"
+//--------------------------------------------------
+#endif /* ARP_H_ */
---
+++ MDK-ARM/Enc28j60/arp.d
@@ -0,0 +1,2 @@
+enc28j60\arp.o: ..\Core\Src\arp.c
+enc28j60\arp.o: ../Core/Inc/arp.h
